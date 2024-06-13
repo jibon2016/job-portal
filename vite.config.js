fdd86eca1ee63d6085f0fdcab827d926e5bc09ea
sd_jibon <jibon.developer@gmail.com>
2024-06-13 11:14:42 +0600

    Update CI/CD file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            'ziggy-js': path.resolve('vendor/tightenco/ziggy/dist/vue.es.js'),
+        }
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
