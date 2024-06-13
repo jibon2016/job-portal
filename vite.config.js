7bb19c761ae79e0299e36420d875162b7c448658
sd_jibon <jibon.developer@gmail.com>
2024-06-13 11:19:26 +0600

    Update CI/CD file

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     resolve: {
         alias: {
-            'ziggy-js': path.resolve('vendor/tightenco/ziggy/dist/vue.es.js'),
+            'ziggy-js': path.resolve('./vendor/tightenco/ziggy/dist/index.esm.js'),
         }
     },
     plugins: [
